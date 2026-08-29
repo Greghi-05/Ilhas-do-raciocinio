83706ff83412efebf2ab7378e588a3a967067465
Lanna <lannagreghiamaro@gmail.com>
2026-08-29 20:00:27 -0300

    First level

--- scripts/character.gd
+++ scripts/character.gd
@@ -1,31 +1,21 @@
 extends CharacterBody2D
 
-# --- ESPÉCIE ---
 @export_enum("porco", "gato", "vaca", "sapo", "urso", "pinto") var especie: String = "porco"
 
-# --- ÓCULOS ---
 @export var usa_oculos: bool = false
-@export_enum("nenhuma", "azul", "vermelho", "verde", "amarelo") var cor_oculos: String = "nenhuma"
+@export_enum("nenhuma", "azul", "vermelho", "verde", "amarelo", "roxo") var cor_oculos: String = "nenhuma"
 
-# --- SAPATOS ---
 @export var usa_sapato: bool = false
-@export_enum("nenhuma", "azul", "vermelho", "verde", "amarelo") var cor_sapato: String = "nenhuma"
+@export_enum("nenhuma", "azul", "vermelho", "verde", "amarelo", "roxo") var cor_sapato: String = "nenhuma"
 
-# --- ACESSÓRIO DE CABEÇA ---
 @export var usa_acessorio_cabeca: bool = false
-@export_enum("nenhuma", "azul", "vermelho", "verde", "amarelo") var cor_acessorio_cabeca: String = "nenhuma"
+@export_enum("nenhuma", "azul", "vermelho", "verde", "amarelo", "roxo") var cor_acessorio_cabeca: String = "nenhuma"
 
 
 func _ready():
-	# Imprime no console exatamente como esse personagem foi configurado no Inspector!
 	print("--- NOVO PERSONAGEM CARREGADO ---")
 	print("Espécie: ", especie)
 	print("Usa óculos? ", usa_oculos, " | Cor do óculos: ", cor_oculos)
 	print("Usa sapato? ", usa_sapato, " | Cor do sapato: ", cor_sapato)
 	print("Usa acessório na cabeça? ", usa_acessorio_cabeca, " | Cor do acessório: ", cor_acessorio_cabeca)
 	print("---------------------------------")
-
-
-func _physics_process(delta):
-	# Mantive o movimento simples que você tinha feito no seu script original
-	position.x += 100 * delta
--- scripts/goal_area.gd
+++ scripts/goal_area.gd
@@ -1,10 +1,21 @@
 extends Area2D
 
+@export_enum("apenas_porco", "tem_acessorio", "tem_oculos") var regra_do_caminho: String = "apenas_porco"
+
 func _ready():
 	body_entered.connect(_on_body_entered)
 
 func _on_body_entered(body):
-	if body.color == "blue":
-		print(body.character_name, "PASSOU")
-	else:
-		print(body.character_name, "BLOQUEADO")
+	if "especie" in body:
+		var passou = false
+		if regra_do_caminho == "apenas_porco" and body.especie == "porco":
+			passou = true
+		elif regra_do_caminho == "tem_acessorio" and body.usa_acessorio_cabeca == true:
+			passou = true
+		elif regra_do_caminho == "tem_oculos" and body.usa_oculos == true:
+			passou = true
+			
+		if passou:
+			print("PASSOU!")
+		else:
+			print("BLOQUEADO!")
